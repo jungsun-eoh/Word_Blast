2df0bb67987f24780641dd86ad22eaaf30a7cb0c
jungsun-eoh <jungsun.eoh@gmail.com>
2020-03-13 20:03:10 -0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/Word_Blast.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Word_Blast.dir/main.c.o"
+  "Word_Blast"
+  "Word_Blast.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Word_Blast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Word_Blast.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Word_Blast.
+# This may be replaced when dependencies are built.
