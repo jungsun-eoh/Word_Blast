463479c97b88d392dd608123eef7606e04a5ec29
jungsun-eoh <jungsun.eoh@gmail.com>
2020-03-25 22:43:05 -0700

    d

--- main.c
+++ main.c
@@ -29,7 +29,7 @@
 // easy to change spec in one place
 #define MINCHARS 6
 #define TOP 10
-#define SIZEBLOCK 2500
+#define SIZEBLOCK 400000
 
 #define debug_print(fmt, ...) \
             do { if(DEBUG_TEST) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
@@ -39,13 +39,10 @@ typedef struct wordFrequency {
     int frequency;      // value
 } wordFrequency, *wordFrequency_p;
 
-wordFrequency_p wordTable = NULL;
-int maxWordTable = 0;  //size of the array
+volatile wordFrequency_p wordTable = NULL;
+volatile int maxWordTable = 0;  //size of the array
 int countWordTable = 0;     // number of value in the array
 
-int count =0;
-
-long taskSize = 0;
 char* fileName;
 int numberOfThread = 0;
 
@@ -56,10 +53,11 @@ void add_word(char* w);
  * So the hash table will keep tracking how many times the word appeared in the .txt file.
  */
 void* threadWorking(void *);
-void getTaskSize(void);
+long getTaskSize(int);
 
 int main(int argc, char *argv[])
 {
+
     pthread_t* threadPT;
     int* index;
 
@@ -74,7 +72,6 @@ int main(int argc, char *argv[])
     {
         fileName = argv[1];
         numberOfThread = atoi(argv[2]);
-        getTaskSize();
     }
     else
         {
@@ -82,7 +79,7 @@ int main(int argc, char *argv[])
         return (-1);
         }
 
-    index = malloc(sizeof(long) * numberOfThread);
+    index = malloc(sizeof(int) * numberOfThread);
     threadPT = malloc(sizeof(pthread_t) * numberOfThread);
 
     for(int i =0; i<numberOfThread; i++)
@@ -95,8 +92,39 @@ int main(int argc, char *argv[])
         pthread_join(threadPT[i], NULL);
     }
 
+    // find the top list
+    wordFrequency top10[TOP];
+    for (int j = 0; j < TOP; j++) {
+        top10[j].frequency = 0;
+    }
+
+    for(int j = 0;  j< countWordTable; j++)
+    {
+        if(wordTable[j].frequency > top10[TOP-1].frequency)
+        {
+            //debug_print("TOP %d. Adding %s %d\n", TOP, wordTable[j].word, wordTable[j].frequency);
+
+            top10[TOP-1].word = wordTable[j].word;
+            top10[TOP-1].frequency = wordTable[j].frequency;
+
+            for(int p = TOP-2; p >=0; p-- )
+            {
+                if (wordTable[j].frequency > top10[p].frequency)
+                {
+                    top10[p+1].word = top10[p].word;
+                    top10[p+1].frequency = top10[p].frequency;
+                    top10[p].word = top10[j].word;
+                    top10[p].frequency = top10[j].frequency;
+                }
+            }
+        }
+    }
+
     printf("\n Word frequency count on %s with %d threads\n", fileName, numberOfThread);
     printf("printing top %d words %d char or more.\n", TOP, MINCHARS);
+
+    for(int a = 0; a < TOP; a++)
+        printf ("Number %d is %s with a count of %d", a+1, top10[a].word, top10[a].frequency);
 
     clock_gettime(CLOCK_REALTIME, &endTime);
     time_t sec = endTime.tv_sec - startTime.tv_sec;
@@ -114,7 +142,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void getTaskSize(void)
+long getTaskSize(int numberOfThread)
 {
     int fileDescripter;
     long fileLen;
@@ -128,23 +156,26 @@ void getTaskSize(void)
         fileLen = lseek(fileDescripter, 0, SEEK_CUR);
         lseek(fileDescripter, 0, SEEK_SET);
         close(fileDescripter);
-
-        taskSize = fileLen / numberOfThread;
         //printf("Size of file: %ld bytes\n", taskSize);
     }
+    return fileLen / numberOfThread;
 }
 
 void *threadWorking(void *arg)
 {
+    long taskSize = getTaskSize(numberOfThread);
     int index_i = *(int *)arg;
     int fileDescripter;
     char *buf;
     char *word;
     char *saveptr;
     char * delim = "\"\'.""''?:;-,—*($%)! \t\n\x0A\r";
+    //long filelen = lseek(fileDescripter, 0, SEEK_CUR);
+
+    long start;
 
     // need extra 4 for wide null
-    buf = malloc((sizeof(char) * taskSize) +4);
+    buf = malloc((sizeof(long) * taskSize) +4);
 
     if(buf ==NULL)
     // buffer error indicator
@@ -161,12 +192,17 @@ void *threadWorking(void *arg)
         return NULL;
     }
 
+    start = index_i * taskSize;
     // read amount of task from main and store into buf
     //because we open the file read-only, we don't need mutex for race condition.
-    lseek(fileDescripter, index_i * taskSize, SEEK_SET);
+    lseek(fileDescripter, start, SEEK_SET);
 
-    ////////////////////////*if ()*///////////////////
-    long res = read(fileDescripter, buf, taskSize);
+//    if (index_i == numberOfThread-1)
+//    {
+//        taskSize = filelen - (index_i * taskSize);
+//    }
+    long res;
+    res = read(fileDescripter, buf, taskSize);
     debug_print("On thread %d: asked for %ld bytes, got %ld bytes\n", index_i,taskSize, res);
 
     close(fileDescripter);
@@ -187,42 +223,16 @@ void *threadWorking(void *arg)
         {
             add_word(word);
             //debug_print("%d word :: %s\n", index_i, word);
-
         }
         word = strtok_r(NULL, delim, &saveptr);
-
     }
-
-
-
 
     free(buf);
     return NULL;
 }
-void add_word(char* w) {
 
-    //debug_print("word :: %s\n", w);
-    if(wordTable == NULL)  // first input.
-    {
-        pthread_mutex_lock(&lock);
-        wordTable = malloc(sizeof(wordFrequency) * SIZEBLOCK);
-        maxWordTable = SIZEBLOCK;
-        if (wordTable == NULL)
-        {
-            printf("first init malloc error in addWord\n");
-            exit(-2);
-        }
-        // w is char pointer, and pointer could change it. we have to make new heap memory to store
-        // word to always have valid value.
-        debug_print("new word %s\n", w);
-        wordTable[0].word = malloc(strlen(w) +2);
-        strcpy(wordTable[0].word, w);  //copy the word into wordTable
-        wordTable[0].frequency = 1;
-        countWordTable = 1;
-        pthread_mutex_unlock(&lock);
-    }else   // wordtable already exists.
-    {
-        for(int i =0; i < countWordTable; i ++)        // find the word in the table
+void add_word(char* w) {
+    for(int i =0; i < countWordTable; i ++)        // find the word in the table
         {
             if (strcasecmp(wordTable[i].word, w) == 0)
             {
@@ -232,31 +242,45 @@ void add_word(char* w) {
                 return;
             }
         }
-        if(maxWordTable <= countWordTable)
+    pthread_mutex_lock(&lock);
+    //debug_print("maxWordTable: %d\n countWordTable: %d\n", maxWordTable, countWordTable);
+
+    if(maxWordTable <= countWordTable)
             // if we don't have enough room for the table, we reallocate malloc.
+    {
+        maxWordTable = maxWordTable + SIZEBLOCK;
+        if(wordTable == NULL)  // first input.
         {
-            pthread_mutex_lock(&lock);
-            maxWordTable = maxWordTable + SIZEBLOCK;
-            wordFrequency_p reallocRet = realloc(wordTable, sizeof(wordFrequency) * maxWordTable);
+            wordTable = malloc(sizeof(wordFrequency) * maxWordTable);
+            if (wordTable == NULL) {
+                printf("first init malloc error in addWord\n");
+                exit(-2);
+            }
+            // w is char pointer, and pointer could change it. we have to make new heap memory to store
+            // word to always have valid value.
+        } else
+        {
+            wordFrequency_p reallocRet = realloc (wordTable, sizeof(wordFrequency) * maxWordTable);
             if (reallocRet == NULL)
             {
                 printf("realloc malloc error in addWord\n");
                 exit(-2);
             }
             wordTable = reallocRet;
-            pthread_mutex_unlock(&lock);
         }
-        // add the new word
-        pthread_mutex_lock(&lock);
-
-        wordTable[countWordTable].word = malloc (strlen(w) +2);
-        strcpy(wordTable[countWordTable].word, w);  //copy the word into wordTable
-        wordTable[countWordTable].frequency = wordTable[countWordTable].frequency + 1;
-        ++countWordTable;
-        pthread_mutex_unlock(&lock);
-
-        debug_print("new word2 %s\n", w);
     }
+    // add the word to the list
+
+    wordTable[countWordTable].word = malloc(strlen(w) + 2);
+    if (wordTable[countWordTable].word == NULL) {
+        printf("word malloc error in addWord\n");
+        exit(-2);
+    }
+    strcpy(wordTable[countWordTable].word, w);  //copy the word into wordTable
+    wordTable[countWordTable].frequency = 1;
+    ++countWordTable;
+
+    pthread_mutex_unlock(&lock);
 
     return;
 }
